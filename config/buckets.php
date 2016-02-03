7f27fec516a3311d0d1c2e23bfb1719b48caf49a
Cartwrights Computer <dominic.cartwright07@gmail.com>
2016-02-03 18:59:34 -0500

    Fixed a mistake. Add one to many '/'.

--- config/buckets.php
+++ config/buckets.php
@@ -7,14 +7,14 @@
     ),
     'template' => array(
       'web_path'  => '/bvg-admin/extras/img/template',
-      'file_path' => '/Applications/MAMP/htdocs//bvg-admin/extras/img/template'
+      'file_path' => '/Applications/MAMP/htdocs/bvg-admin/extras/img/template'
     ),
     'home' => array(
       'web_path'  => '/bvg-admin/extras/img/home',
-      'file_path' => '/Applications/MAMP/htdocs//bvg-admin/extras/img/home'
+      'file_path' => '/Applications/MAMP/htdocs/bvg-admin/extras/img/home'
     ),
     'about' => array(
       'web_path'  => '/bvg-admin/extras/img/about',
-      'file_path' => '/Applications/MAMP/htdocs//bvg-admin/extras/img/about'
+      'file_path' => '/Applications/MAMP/htdocs/bvg-admin/extras/img/about'
     )
   );
