64c9511f0ceeb30f6122762137356199554c1345
Dominic Cartwright <dominic.cartwright07@gmail.com>
2016-02-24 21:03:49 -0500

    Add the new design to the footer subscription area.

--- templates/layouts/global/footer.php
+++ templates/layouts/global/footer.php
@@ -12,8 +12,54 @@
       </div>
 
       <div class="col_one_third col_last">
+        <div class="widget quick-contact-widget clearfix">
 
-        <?php perch_content('Footer Subscription'); ?>
+          <h4>Subscribe to Our Newsletter</h4>
+          <div id="quick-contact-form-result" data-notify-type="success" data-notify-msg="<i class=icon-ok-sign></i> Message Sent Successfully!"></div>
+          <form id="quick-contact-form" name="quick-contact-form" action="fpp/index.php" method="post" class="quick-contact-form nobottommargin">
+            <div class="form-process"></div>
+
+            <div class="input-group divcenter">
+              <span class="input-group-addon"><i class="icon-user"></i></span>
+              <input type="text" class="required form-control input-block-level" id="quick-contact-form-name" name="quick-contact-form-name" value="" placeholder="Name" />
+            </div>
+
+            <div class="input-group divcenter">
+              <span class="input-group-addon"><i class="icon-email2"></i></span>
+              <input type="text" class="required form-control email input-block-level" id="quick-contact-form-email" name="quick-contact-form-email" value="" placeholder="Email Address" />
+            </div>
+
+            <div class="input-group btn-group divcenter dropup">
+              <button class="btn btn-default form-control required dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                <span class="valueOfButton">Choose An Island</span>
+                <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <li>Abaco</li>
+                <li>Acklins</li>
+                <li>Andros</li>
+                <li>Berry Islands</li>
+                <li>Bimini</li>
+                <li>Cat Island</li>
+                <li>Crooked Island</li>
+                <li>Eleuthera</li>
+                <li>Exuma</li>
+                <li>Grand Bahama</li>
+                <li>Inagua</li>
+                <li>Long Island</li>
+                <li>Mayaguana</li>
+                <li>New Providence</li>
+                <li>Rum Cay</li>
+                <li>San Salvador</li>
+              </ul>
+            </div>
+
+            <!-- <textarea class="required form-control input-block-level short-textarea" id="quick-contact-form-message" name="quick-contact-form-message" rows="4" cols="30" placeholder="Message"></textarea> -->
+            <input type="text" class="hidden" id="quick-contact-form-botcheck" name="quick-contact-form-botcheck" value="" />
+            <button type="submit" id="quick-contact-form-submit" name="quick-contact-form-submit" class="btn btn-success nomargin" value="submit">Subscribe</button>
+          </form>
+        </div>
+
         <?php perch_content('Footer Twitter Feed'); ?>
 
       </div>
