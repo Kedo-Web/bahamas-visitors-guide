24d78cfa0d67eb870ba9e987ca2eed15593f4ce2
Dominic Cartwright <dominic.cartwright07@gmail.com>
2016-04-29 18:03:04 -0400

    - fixed the search functionality on the webiste

--- templates/pages/search.php
+++ templates/pages/search.php
@@ -1,21 +1,63 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-	<title>Perch Example Search Page</title>
-	<?php perch_get_css(); ?>
-</head>
-<body>
-	<div class="wrapper cols2-nav-right">
-		<div class="primary-content">
-		    <?php 
-			    perch_content_search(perch_get('q'));
-			?>
+<?php
+  perch_layout('global/header', array(
+    'body-class' => 'stretched sticky-responsive-menu',
+  ));
+?>
+
+<!-- Header ============================================= -->
+<header id="header" class="sticky-style-2">
+  <div class="container clearfix desktop">
+    <!-- Logo ============================================= -->
+    <?php perch_content('Site Logo'); ?>
+
+    <?php perch_content('Banner Ad'); ?>
+  </div>
+
+  <div id="header-wrap">
+    <!-- Primary Navigation ============================================= -->
+    <nav id="primary-menu" class="style-2">
+      <div class="container clearfix">
+        <a id="mmenu-btn" href="#mobile-menu" class="hideDesktop"><i class="fa fa-navicon"></i></a>
+        <a id="mobile-menu-logo" href="/">
+          <img src="/bvg-admin/extras/img/template/bvg-logo.png" alt="Bahamas Visitor's Guide Logo">
+        </a>
+        <?php perch_pages_navigation(array(
+          'navgroup' =>'top-menu',
+          'levels' => 1
+        )); ?>
+
+        <!-- Top Search ============================================= -->
+        <div id="top-search" class="no-home">
+          <a href="#" id="top-search-trigger">
+            <i class="fa fa-search"></i>
+            <i class="fa fa-times"></i>
+          </a>
+          <?php perch_search_form(array(
+						'template'	=> 'my-search-form.html',
+					)); ?>
+        </div><!-- #top-search end -->
+      </div>
+    </nav><!-- #primary-menu end -->
+  </div>
+</header><!-- #header end -->
+
+<?php perch_content('Page Title'); ?>
+
+<!-- Content ============================================= -->
+<section id="content">
+  <div class="content-wrap">
+    <div class="container clearfix">
+      <?php
+        perch_search_form(array(
+          'template'	=> 'search-form.html',
+        ));
+        $query = perch_get('q');
+        perch_content_search($query);
+      ?>
 		</div>
-		<nav class="sidebar">
-		   <?php perch_search_form(); ?>
-    	</nav>
 	</div>
-    <?php perch_get_javascript(); ?>
-</body>
-</html>
+</section>
+
+<?php
+  perch_layout('global/footer');
+?>
